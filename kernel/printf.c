6e7ad0f720408b7d6432f255d78c601918fc8c82
forbiddengame <panxiezhao@gmail.com>
2023-04-03 19:41:11 +0800

    lab trap bp solution

--- kernel/printf.c
+++ kernel/printf.c
@@ -121,6 +121,7 @@ panic(char *s)
   printf("panic: ");
   printf(s);
   printf("\n");
+  backtrace();
   panicked = 1; // freeze uart output from other CPUs
   for(;;)
     ;
@@ -131,4 +132,19 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void
+backtrace(void)
+{
+  uint64 fp = r_fp();
+  uint64 bottom, top;
+  bottom = PGROUNDDOWN((uint64)fp);
+  top = PGROUNDUP((uint64)fp);
+
+  while (bottom < fp && fp < top) {
+    printf("%p\n", *((uint64 *) (fp - 8)));
+    fp = *((uint64 *) (fp - 16));
+  }
+  return;
 }
