2da9ea95c6cbb535711c4a12e1689070f19cabc1
Fg <panxiezhao@gmail.com>
2023-04-25 14:50:31 +0800

    debug

--- user/uthread.c
+++ user/uthread.c
@@ -10,15 +10,50 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
-
+struct context {
+    uint64 ra;
+    uint64 sp;
+    
+    // callee-saved
+    uint64 s0;
+    uint64 s1;
+    uint64 s2;
+    uint64 s3;
+    uint64 s4;
+    uint64 s5;
+    uint64 s6;
+    uint64 s7;
+    uint64 s8;
+    uint64 s9;
+    uint64 s10;
+    uint64 s11;
+};
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct context context;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
 extern void thread_switch(uint64, uint64);
-              
+
+
+static inline uint64
+r_sp()
+{
+  uint64 x;
+  asm volatile("mv %0, sp" : "=r" (x) );
+  return x;
+}
+
+static inline uint64
+r_ra()
+{
+  uint64 x;
+  asm volatile("mv %0, ra" : "=r" (x) );
+  return x;
+}
+
 void 
 thread_init(void)
 {
@@ -62,6 +97,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)&t->context, (uint64)&current_thread->context);
   } else
     next_thread = 0;
 }
@@ -76,12 +112,17 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->context.ra = (uint64)func;
+  t->context.sp = (uint64)&t->stack;
 }
 
 void 
 thread_yield(void)
 {
   current_thread->state = RUNNABLE;
+  current_thread->context.ra = r_ra();
+  current_thread->context.sp = r_sp();
+
   thread_schedule();
 }
 
